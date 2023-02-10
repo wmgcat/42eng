3de5510f93f67acabe216b2e91702ae27081915b
wmgcat <twiircl@gmail.com>
2023-02-10 23:15:44 +0300

    new yandex module

--- modules/yandex.js
+++ modules/yandex.js
@@ -131,6 +131,26 @@ modules.yandex = {
         return Promise.resolve(state);
       }
     }
+  },
+  metrika: { // yandex.metrika:
+    id: 0,
+    init: id => {
+      modules.yandex.metrika.id = id;
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      var z = null;m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      ym(modules.yandex.metrika.id, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true
+      });
+    },
+    goal: name => {
+      ym(modules.yandex.metrika.id, 'reachGoal', name);
+      Add.debug('reach is goal!', name);
+    }
   }
 };
 /*YA = {
