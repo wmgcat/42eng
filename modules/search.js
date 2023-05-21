901229a8908e82b137d23914a4fbed6df7476cb2
Мажик <twiircl@gmail.com>
2023-05-21 17:53:53 +0300

    fix search module

--- modules/search.js
+++ modules/search.js
@@ -7,7 +7,7 @@
 /**
  * Модуль поиска
  * @namespace
- */
+*/
 const search = new Module('search', '1.2');
 
 /**
@@ -19,7 +19,7 @@ const search = new Module('search', '1.2');
  * @param  {number} distance Дистанция
  * @param  {number} [offset=0] Отступ от искомого объекта
  * @return {array}
- */
+*/
 search.distance = (name, x, y, distance, offset=0) => {
   if (!modules.math) return false;
   return modules.search.search(name).filter(obj => (modules.math.distance(x, y, obj.x + offset, obj.y + offset) <= distance));
@@ -30,12 +30,13 @@ search.distance = (name, x, y, distance, offset=0) => {
  * 
  * @param  {arguments[]} id ID объектов, можно искать несколько
  * @return {array}
- */
+*/
 search.id = (...args) => {
   let arr = [];
-  for (const id of args) {
-    if (objects[id]) arr.push(objects[id]);
-  }
+  for (const id of args)
+    if (objects[id])
+      arr.push(objects[id]);
+
   return arr;
 }
 
@@ -44,22 +45,22 @@ search.id = (...args) => {
  * 
  * @param  {arguments[]} name Имя объекта, можно указать несколько
  * @return {array}
- */
-search.search = (...args) => {
+*/
+search.search = (...args) => (
   return Object.keys(objects).filter(x => {
     for (const name of args) {
       if (name == 'all' || objects[x].name == name)
         return true;
     }
   }).map(x => objects[x]);
-}
+);
 
 /**
  * Показывает кол-во объектов по имени
  * 
  * @param  {arguments[]} name Имя объекта, можно указать несколько
  * @return {number}
- */
+*/
 search.count = (...args) => modules.search.search(args).length;
 
 /**
@@ -69,5 +70,5 @@ search.count = (...args) => modules.search.search(args).length;
  * @param  {string} key Параметр для поиска
  * @param  {any} value Значение параметра
  * @return {array}
- */
+*/
 search.key = (name, key, value) => modules.search.search(name).filter(x => (x[key] == value));
