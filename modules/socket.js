6124d66dc928e4029fbb1014f9579fc63d824ae8
Мажик <twiircl@gmail.com>
2023-05-26 10:53:03 +0300

    update socket module

--- modules/socket.js
+++ modules/socket.js
@@ -1,21 +1,84 @@
-modules.socket = {
-  title: 'socket', v: '1.0',
-  init: (ip='localhost', port=8080, update) => {
-    this.ip = ip, this.port = port, this.is_open = true;
-    this.socket = new WebSocket(`ws://${ip}:${port}`);
-    this.socket.onerror = e => { Add.debug(e.message); }
-    this.socket.onopen = e => { Add.debug('server is connected!'); }
-    this.socket.onmessage = e => { update(JSON.parse(e.data)); }
-    this.socket.onclose = e => {
-      if (e.wasClean) Add.debug('clear disconnect!');
-      else Add.debug('not clear disconnect!');
-      this.is_open = false;
+/**
+ * @file Модуль сокетов
+ * @author wmgcat
+ * @version 1.1
+*/
+
+const socket = new Module('socket', '1.1');
+
+/**
+ * Создает объект класса Socket
+ * 
+ * @param  {string} ip IP адрес веб-сокета
+ * @param  {number} port Порт веб-сокета
+ * @param  {bool} protect Защищенное подключение (WSS)
+ * @return {Socket}
+*/
+socket.create = (ip, port, protect) => (
+  new Socket(ip, port, protect)
+);
+
+/**
+ * Сокет
+ * @contructor
+*/
+class Socket {
+
+  /**
+   * @param  {string} [ip=localhost] IP адрес веб-сокета
+   * @param  {number} [port=8080] Порт веб-сокета
+   * @param  {bool} [protect=false] Защищенное подключение (WSS)
+  */
+  constructor(ip='localhost', port=8080, protect=false) {
+    this.ip = ip;
+    this.port = port;
+    this.protect = protect;
+
+
+    this.isOpen = false;
+    this.socket = null;
+  }
+
+  /**
+   * Подключение к веб-сокету
+   *
+   * @param {function} func Функция, принимающая данные с веб-сокета
+   * @return {bool}
+  */
+  run(func) {
+    if (this.isOpen) return false;
+
+    this.socket = new WebSocket(`${this.protect ? 'wss' : 'wss'}://${this.ip}:${this.port}`);
+    
+    this.socket.onopen = e => {
+      this.isOpen = true;
+      Add.debug(`Подключение к ${this.ip} установлено! Порт: ${this.port}`);
     }
-  },
-  send: (type, data) => {
-    if (this.is_open) {
-      data.TYPE = type;
-      this.socket.send(JSON.stringify(data));
-    } else Add.debug('not connection to server!');
+
+    this.socket.onerror = e => {
+      Add.error(e.message, 0);
+      this.isOpen = false;
+    }
+
+    this.socket.onclose = e => {
+      if (!e.wasClean) Add.error(e.message, 0);
+      this.isOpen = false;
+      Add.debug('Подключение завершено!');
+    }
+
+    this.onmessage = e => func(JSON.parse(e.data));
+  }
+
+  /**
+   * Отправляет данные на веб-сокет
+   * 
+   * @param  {string}
+   * @param  {Object}
+  */
+  send(event, data) {
+    if (!this.isOpen) return Add.error('Подключение закрыто!', 0);
+
+    data.event = event;
+    this.socket.send(JSON.stringify(data));
   }
 }
