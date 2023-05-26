1dc31e801470a138064006b01b68fd67a7c7b566
Мажик <twiircl@gmail.com>
2023-05-26 10:32:22 +0300

    added fix save module

--- modules/save.js
+++ modules/save.js
@@ -1,26 +1,65 @@
-modules.save = {
-  title: 'save', v: '1.0',
-  save: async (data) => {
-    let obj = { '__savetime': Date.now() };
-    for (key in data) {
-      let dt = typeof(data[key]) == 'object' ? JSON.stringify(data[key]) : data[key]; 
-      obj[key] = dt;
-      localStorage.setItem(key, dt);
-      Add.debug(`saving ${key} data: `, dt);
-    }
-    localStorage.setItem('__savetime', obj.__savetime);
-    return obj;
-  },
-  load: key => {
-    if (modules.save.check(key)) {
-      Add.debug(`loading ${key} data: `, localStorage.getItem(key));
-      return localStorage.getItem(key);
-    }
-    return false;
-  },
-  check: key => {
-    if (localStorage.getItem(key)) return true;
-    return false;
-  },
-  clear: key => { localStorage.removeItem(key); }
+/**
+ * @file Модуль сохранений
+ * @author wmgcat
+ * @version 1.1
+*/
+
+const save = new Module('save', '1.1');
+
+/**
+ * Сохраняет данные в localStorage, возвращает объект с данными, которые можно сохранить на сервере
+ * 
+ * @param  {Object} data Данные, которые необходимо сохранить / обновить
+ * @return {Object}
+*/
+save.set = async data => {
+  localStorage.setItem('__savetime', Date.now());
+
+  const obj = {};
+  for (let [key, value] of Object.entries(data)) {
+    value = typeof(value) == 'object' ? JSON.stringify(value) : value;
+
+    localStorage.setItem(key, value);
+    obj[key] = value;
+  }
+  return obj;
+}
+
+/**
+ * Получает данные из localStorage, возвращает объект с данными или false
+ * 
+ * @param  {...string} keys Ключ, можно указать несколько
+ * @return {Object}
+*/
+save.get = function(...keys) {
+  const obj = {};
+  for (const key of keys) {
+    const value = this.check(key);
+    if (value == null) continue;
+
+    obj[key] = value;
+  }
+  return obj;
+}
+
+/**
+ * Проверяет есть ли ключ в localStorage
+ * 
+ * @param  {string} key Ключ
+ * @return {bool}
+*/
+save.check = key => {
+  return localStorage.getItem(key);
+}
+
+/**
+ * Очищает данные из localStorage, если аргументов нет - удаляет все данные!
+ * 
+ * @param  {...string} keys Ключ, можно указать несколько
+*/
+save.clear = function(...keys) {
+  if (!keys) return localStorage.clear();
+
+  for (const key of keys)
+    localStorage.removeItem(key);
 }
